153974c8a2e170cb6d2582498f767abd87311ac8
mreteam <rakeshwork98@gmail.com>
2026-01-30 16:58:47 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
-    distDir:"dist",
+    
     images:{
         remotePatterns:[{protocol:"https", hostname:"fakestoreapi.com"}],
-        unoptimized:true
+
     }
 };
 
